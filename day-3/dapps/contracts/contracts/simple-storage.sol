64213fd8ecf1df23762856f7ee78f1124aa2a2a8
Janwar05 <135832357+Janwar05@users.noreply.github.com>
2026-01-16 22:55:05 +0700

    Add files via upload

---
+++ day-3/dapps/contracts/contracts/simple-storage.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SimpleStorage {
+    uint256 private storedValue;
+
+    address public owner;
+
+    event OwnerSet(address indexed newOwner);
+    event ValueUpdated(uint256 newValue);
+
+    constructor() {
+        owner = msg.sender;
+        emit OwnerSet(owner);
+    }
+
+    function setValue(uint256 _value) public {
+        storedValue = _value;
+        emit ValueUpdated(_value);
+    }
+
+    function getValue() public view returns (uint256) {
+        return storedValue;
+    }
+}
